0645109f1584ce79f5972bb3617821af92b2a8eb
Martin Castillo <marencas@gmail.com>
2016-11-27 00:29:09 -0500

    added db connection

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First name', validators=[DataRequired("Must enter a first name")])
+    last_name = StringField('Last name', validators=[DataRequired("Must enter a last name")])
+    email = StringField('Email', validators=[DataRequired("Must enter a valid email address"), Email("Incorrect format for email")])
+    password = PasswordField('Password', validators=[DataRequired("Password is incorrect"), 
+    												 Length(min=6, message="Password must be 6 characters or more")])
     submit = SubmitField('Sign up')
--- routes.py
+++ routes.py
@@ -1,17 +1,28 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignupForm
+
+'''
+# Documentation
+## This python file contains the controller of the application
+## and it points to each of the html files that are found in 
+## the ./templates folder. It does have some logic built in and
+## in order to bring up the page it uses the render_template function
+## that takes in the name of the html file.
+'''
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+# Connecting to postgresql database.  Needs a username and password in 
+# this format -> username:password@localhost:5432/databasename
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tedo_admin:Oct1st!6@localhost:5432/learningflask'
 db.init_app(app)
 
 app.secret_key = "development-key"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index1.html")
 
 @app.route("/about")
 def about():
@@ -22,7 +33,16 @@ def signup():
     form = SignupForm()
 
     if request.method == 'POST':
-        return "Success!"
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            newuser = User(form.first_name.data,
+                           form.last_name.data,
+                           form.email.data,
+                           form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "Success!"
 
     elif request.method == 'GET':
         return render_template('signup.html', form=form)
